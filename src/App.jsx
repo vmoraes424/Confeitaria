23cd969e8be622224780a3d4108c54551ad269cc
Vinicius Moraes <vmoraes424@gmail.com>
2023-06-22 15:30:17 -0300

    Adicionando funcionalidades e refinando detalhes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import "./App.css";
+import Swal from "sweetalert2";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [receitas, setReceitas] = useState([]);
+  const [nome, setNome] = useState("");
+  const [ingredientes, setIngredientes] = useState("");
+  const [modo, setModo] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const receita = {
+      nome,
+      ingredientes,
+      modo,
+    };
+    if (nome === "" || ingredientes === "" || modo === "") {
+      Swal.fire("Erro", "Preencha todos os campos", "error");
+      return;
+    } else {
+      setReceitas([...receitas, receita]);
+      setNome("");
+      setIngredientes("");
+      setModo("");
+    }
+  }
+
+  const receitada = receitas.map((receita) => (
+    <div className="receita">
+      <h2>{receita.nome}</h2>
+    </div>
+  ));
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav />
+      <main>
+        <div className="esquerda">
+          <div className="cima">
+            <h1>Cadastre uma nova receita</h1>
+            <img src="./public/imgs/livro.png" alt="" />
+          </div>
+          <form action="" onSubmit={handleSubmit}>
+            <label htmlFor="nome">Nome da receita</label>
+            <input
+              type="text"
+              name="nome"
+              value={nome}
+              id="nome"
+              onChange={(e) => setNome(e.target.value)}
+            />
+            <label htmlFor="ingredientes">Ingredientes</label>
+            <textarea
+              name="ingredientes"
+              id="ingredientes"
+              cols="30"
+              value={ingredientes}
+              rows="10"
+              onChange={(e) => setIngredientes(e.target.value)}
+            ></textarea>
+            <label htmlFor="modo">Modo de preparo</label>
+            <textarea
+              name="modo"
+              id="modo"
+              cols="30"
+              value={modo}
+              rows="10"
+              onChange={(e) => setModo(e.target.value)}
+            ></textarea>
+            <button type="submit">Adicionar</button>
+          </form>
+        </div>
+        <div className="direita">
+          <h1>Receitas</h1>
+          <div className="receitas">
+            {receitas.length === 0 ? (
+              <h1>Nenhuma receita cadastrada</h1>
+            ) : (
+              receitada
+            )}
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <nav>
+      <img src="./public/imgs/logo.png" alt="" />
+      <h1>Confeitaria Tudibao</h1>
+    </nav>
+  );
+}
